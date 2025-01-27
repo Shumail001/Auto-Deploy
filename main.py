d99163506e65a10c70c8345c503fb4b5116d8814
Usama Liaqat <m.usama.liaqat@gmail.com>
2025-01-27 17:31:02 +0500

    changings in .env

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ sqs_client = boto3.client("sqs",
                           region_name=os.getenv("AWS_DEFAULT_REGION"),
                           aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
                           aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
-                          )  # Replace with your region
-sqs_queue_url = "https://sqs.us-east-1.amazonaws.com/054037101129/Video-Editor-Queue.fifo"
+                          )
+sqs_queue_url = os.getenv("SQS_QUEUE_URL")
 
 def send_to_sqs(creative_id: str, data: dict, s3_bucket: str, file_basename: str, review_id: int):
 
