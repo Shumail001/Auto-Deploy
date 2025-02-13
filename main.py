1e07e97dcd099532497bb2d47d0bd293e3ab1560
Shumail001 <125453898+Shumail001@users.noreply.github.com>
2025-02-13 15:18:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uuid
 from dotenv import load_dotenv
 import boto3
 from fastapi import FastAPI, HTTPException
-
+app = FastAPI(title="Leonardo Video Editor")
 # Home route
 @app.get("/")
 def home():
